e2db24859309666904f97ec3a6dd6042c4c60bfd
rujulak <115549599+rujulak@users.noreply.github.com>
2023-09-15 12:38:03 +0530

    Initialization of state of lander

--- Incubyte_problem.cpp
+++ Incubyte_problem.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main()
 {
     cout<<"hello moon";
+    int xStart = 0, yStart = 0, zStart = 0;
+    int dirStart  = 'N'
+    Lander chandrayan(xStart, yStart, zStart, dirStart);
     return 0;
 }
