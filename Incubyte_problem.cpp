2860ded095e37f72a892fafa7519fc96a9710e32
rujulak <115549599+rujulak@users.noreply.github.com>
2023-09-15 12:46:45 +0530

    creating a class for determining the state properties of the lander

--- Incubyte_problem.cpp
+++ Incubyte_problem.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Lander
+{
+    private:
+    int x,y,z;
+    char direction;
 
+    public:
+    Lander(int x0, int y0, int z0, char dir0)
+    {
+        x = x0;
+        y = y0;
+        z = z0;
+        direction = dir0;
+    }
+};
 int main()
 {
     cout<<"hello moon";
     int xStart = 0, yStart = 0, zStart = 0;
-    int dirStart  = 'N'
+    int dirStart  = 'N';
     Lander chandrayan(xStart, yStart, zStart, dirStart);
     return 0;
 }
